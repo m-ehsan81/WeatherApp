5db7a3e22939bfaccfab922d4f7b6947c626270b
ehsan <m.ehsan1381@gmail.com>
2024-08-16 23:03:35 +0330

    get current location and change style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { AppDispatch } from "./store";
+import { fetchWheatherData, weatherActions } from "./store/weather-slice";
+import ContainerBox from "./components/containerBox";
+
 export default function App() {
-  return <div></div>;
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    (async function fetchData() {
+      try {
+        const response = await fetch("http://ip-api.com/json");
+        const data = await response.json();
+        dispatch(weatherActions.setCurrentLocation(data.city));
+        dispatch(fetchWheatherData(data.city));
+      } catch (error) {
+        dispatch(weatherActions.setError("Failed to fetch"));
+      }
+    })();
+  }, []);
+
+  return <ContainerBox />;
 }
